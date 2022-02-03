c81c39d0b973937de23951934ccc7572dc5787ae
jungho shin <wjdghtls11@gmail.com>
2022-02-03 16:23:52 +0900

    Fix: boardProject 환경변수 설정 및 각 코드에 적용

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -5,6 +5,9 @@ import { ExtractJwt ,Strategy } from 'passport-jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { userRepository } from './user.repository';
 import { User } from './user.entity';
+import * as config from 'config';
+
+const jwtConfig = config.get('jwt');
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
@@ -12,7 +15,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     private userRepository: userRepository
   ) {
     super({
-      secretOrKey: 'Secret1234', // 토큰 유효한지 확인할때
+      secretOrKey: process.env.JWT_SECRET || jwtConfig.secret, // 토큰 유효한지 확인할때
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // 토큰이 어디서 오는지 확인하는 것
     })
   }
--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,15 +1,18 @@
 /* eslint-disable prettier/prettier */
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as config from 'config';
+
+const dbConfig = config.get('db');
 
 export const typeORMConfig: TypeOrmModuleOptions = {
-  type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "postgres",
-  database: "Board-app",
+  type: dbConfig.type,
+  host: process.env.RDS_HOSTNAME || dbConfig.host,
+  port: process.env.RDS_PORT || dbConfig.post,
+  username: process.env.RDS_USERNAME || dbConfig.username,
+  password: process.env.RDS_PASSWORD || dbConfig.password,
+  database: process.env.RDS_DB_NAME || dbConfig.database,
   entities: [__dirname + "/../**/*.entity.{js,ts}"],
-  synchronize: true
+  synchronize: dbConfig.synchronize
 }
 
 // entities : 엔티티를 이용해서 데이터베이스 테이블을 생성, 따라서 엔티티 파일이 어디에 있는지 설정
